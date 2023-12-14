643e7c882ac144cc84d8f468b1db2d2e83851787
Vu Minh Vuong <vuongvu1208@gmail.com>
2023-12-14 23:54:49 +0700

    Add testing script

---
+++ tests/test_multi_request.py
@@ -0,0 +1,72 @@
+import asyncio
+import os
+import random
+import sys
+import time
+import logging
+import argparse
+
+import httpx
+
+async def send_request(file):
+    url = f"{args.host}:{args.port}/api/audio/upload"
+    files = {"audio_file": file, "type": "audio/wav"}
+    async with httpx.AsyncClient(timeout=120) as client:
+        r = await client.post(
+            url,
+            files=files,
+        )
+        _logger.info("Response: %d", r.status_code)
+
+if __name__ == "__main__":
+    _logger = logging.getLogger(__file__)
+    handler = logging.StreamHandler(sys.stdout)
+    file_handler = logging.FileHandler("test_multi_request.log")
+    _logger.addHandler(file_handler)
+    _logger.addHandler(handler)
+    _logger.setLevel(logging.INFO)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-ap",
+        "--audio-path",
+        type=str,
+        default="./vivos/test/waves/VIVOSDEV01/VIVOSDEV01_R003.wav",
+        required=False,
+    )
+    parser.add_argument(
+        "-ho",
+        "--host",
+        type=str,
+        default="http://127.0.0.1",
+        required=False,
+    )
+    parser.add_argument(
+        "-p",
+        "--port",
+        type=str,
+        default="8000",
+        required=False,
+    )
+    args = parser.parse_args()
+
+    file_lists = []
+    for file in os.listdir(args.audio_path):
+        f = os.path.join(args.audio_path, file)
+        if os.path.isfile(f):
+            with open(f, 'rb') as f:
+                data = f.read()
+                file_lists.append(data)
+                f.close()
+
+    req = []
+    for i in range(1):
+        file_idx = random.randint(0, len(file_lists) - 1)
+        req.append(send_request(file_lists[file_idx]))
+
+    loop = asyncio.get_event_loop()
+    group = asyncio.gather(*req)
+    start = time.time()
+    loop.run_until_complete(group)
+    end = time.time()
+    _logger.info(f"Time elapsed: {end - start}s")
--- tests/test_response_time.py
+++ tests/test_response_time.py
@@ -9,6 +9,8 @@ import httpx
 if __name__ == "__main__":
     _logger = logging.getLogger(__file__)
     handler = logging.StreamHandler(sys.stdout)
+    file_handler = logging.FileHandler("test_response_time.log")
+    _logger.addHandler(file_handler)
     _logger.addHandler(handler)
     _logger.setLevel(logging.INFO)
 
@@ -36,11 +38,10 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
 
+    _logger.info("File name: ", args.audio_path)
     with open(args.audio_path, "rb") as file:
         url = f"{args.host}:{args.port}/api/audio/upload"
         files = {"audio_file": file, "type": "audio/wav"}
-        # headers = {"filename": os.path.basename(args.audio_path)}
-        # _logger.debug(headers)
         with httpx.Client(timeout=120) as client:
             start = time.time()
             r = client.post(url, files=files)
