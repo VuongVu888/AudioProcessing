fe36d78b108e04c4d09679b21eb2ecc9de27cb16
Vu Minh Vuong <vuongvu1208@gmail.com>
2023-12-14 22:33:17 +0700

    Refactor run script

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export PYTHONPATH=$(pwd)
+ENV_VARS_SCRIPT="set_env_vars.sh"
 
 # Function to gracefully terminate processes
 terminate_processes() {
@@ -13,19 +13,23 @@ terminate_processes() {
 # Trap Ctrl+C (SIGINT) signal to call the termination function
 trap terminate_processes INT
 
-# Start a session for FastAPI
-uvicorn app.main:app --host 0.0.0.0 --port 3000 --workers 4 &
+# Start a tmux session named "mysession"
+tmux new-session -d -s mysession "source $ENV_VARS_SCRIPT && uvicorn app.main:app --host 0.0.0.0 --port 3000 --workers 4"
 
-# Start RabbitMQ consumers
-for i in {1..4}; do
-    python -u inference_workers/workers.py &
-done
+# Store PID of FastAPI process
+fastapi_pid=$!
 
-# Store PIDs of RabbitMQ consumers
-rabbitmq_pids=$(jobs -p)
+# Split the tmux session into 4 panes and start RabbitMQ consumers in each pane
+tmux split-window -t mysession:0.0 "source $ENV_VARS_SCRIPT && python -u inference_workers/workers.py"
+tmux split-window -t mysession:0.1 "source $ENV_VARS_SCRIPT && python -u inference_workers/workers.py"
+tmux split-window -t mysession:0.2 "source $ENV_VARS_SCRIPT && python -u inference_workers/workers.py"
+tmux split-window -t mysession:0.3 "source $ENV_VARS_SCRIPT && python -u inference_workers/workers.py"
 
-# Wait for RabbitMQ consumers to finish
-wait $rabbitmq_pids
+# Set focus on the first pane
+tmux select-pane -t mysession:0.0
+
+# Wait for all processes to finish
+wait
 
 # Terminate processes on script exit
 terminate_processes
---
+++ set_env_vars.sh
@@ -0,0 +1,8 @@
+export PYTHONPATH=$(pwd)
+export RABBITMQ_URL=localhost
+export RABBITMQ_PORT=5672
+export RABBITMQ_USER=root
+export RABBITMQ_PASSWORD=root
+export REDIS_URL=localhost
+export REDIS_PORT=6379
+export MODEL_PATH=./transcription_model/conformer.nemo
